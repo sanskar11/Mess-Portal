f970c21485075484c7ac1c35d1f7eb96af6af0a3
sanskar11 <sanskartibrewal11@gmail.com>
2020-05-26 00:08:46 +0530

    username and password changed

---
+++ dbconfig.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$hostname = "localhost";
+
+$username = "sanskar";
+
+$password = "1234";
+
+$dbname = "test";
+
+$conn = mysqli_connect($hostname, $username, $password);
+
+if (!$conn) {
+
+    die('Could not connect: ' . mysqli_connect_error());
+}
+echo "connection succesful";
+mysqli_select_db($conn, $dbname);
+?>
